86841bde75bc5f7dd9cc5af7a775061d880d3ea5
Rémi Weng <ixartz@users.noreply.github.com>
2021-03-30 19:52:54 +0200

    Initial commit

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 
 import Link from 'next/link';
 
-import { Config } from '../utils/Config';
+import { AppConfig } from '../utils/AppConfig';
 
 type IMainProps = {
   meta: ReactNode;
@@ -16,8 +16,8 @@ const Main = (props: IMainProps) => (
     <div className="max-w-screen-md mx-auto">
       <div className="border-b border-gray-300">
         <div className="pt-16 pb-8">
-          <div className="font-bold text-3xl text-gray-900">{Config.title}</div>
-          <div className="text-xl">{Config.description}</div>
+          <div className="font-bold text-3xl text-gray-900">{AppConfig.title}</div>
+          <div className="text-xl">{AppConfig.description}</div>
         </div>
         <div>
           <ul className="flex flex-wrap text-xl">
@@ -50,7 +50,7 @@ const Main = (props: IMainProps) => (
         {' '}
         {new Date().getFullYear()}
         {' '}
-        {Config.title}
+        {AppConfig.title}
         . Powered with
         {' '}
         <span role="img" aria-label="Love">
