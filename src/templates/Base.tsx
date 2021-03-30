3bec2a70d65027ac30ad3513c2f5ac76bbfee512
Rémi Weng <ixartz@users.noreply.github.com>
2021-03-30 22:23:03 +0200

    Add Base component

---
+++ src/templates/Base.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Base = () => <div className="antialiased text-gray-600" />;
+
+export { Base };
--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,4 +1,4 @@
-export const AppConfig = {
+export const Config = {
   site_name: 'Starter',
   title: 'Nextjs Starter',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
