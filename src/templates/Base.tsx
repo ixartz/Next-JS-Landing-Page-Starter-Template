6126109b171dc907159772bb4dc634e541692fd7
Rémi Weng <ixartz@users.noreply.github.com>
2021-03-31 19:20:51 +0200

    Add meta component

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Meta } from '../layout/Meta';
+import { Config } from '../utils/Config';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
@@ -7,6 +9,7 @@ import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
   <div className="antialiased text-gray-600">
+    <Meta title={Config.title} description={Config.description} />
     <Hero />
     <VerticalFeatures />
     <Banner />
