8eba251ef2e4a0957ea079a382771b0437793859
Rémi Weng <ixartz@users.noreply.github.com>
2021-07-04 19:54:49 +0200

    Update to Next JS 11, feature parity from Next JS Boilerplate

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const baseUrl = '';
-
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: baseUrl,
-  env: {
-    baseUrl: baseUrl,
-  },
-  future: {
-    webpack5: true,
-  },
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
 });
