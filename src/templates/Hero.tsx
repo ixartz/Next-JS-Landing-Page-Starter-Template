caf07677536e3b683558fd9cfd8f27f97dbe3665
AlexandreDevaux <alexandre.devaux@epita.fr>
2023-02-13 23:58:02 +0100

    added cta at the top right corner

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -11,14 +11,11 @@ const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Sign in</a>
+        <li className={'hidden md:block'}>
+          <Link href="tel:0617328806">
+            <a>
+              <Button>📞 Commander maintenant !</Button>
+            </a>
           </Link>
         </li>
       </NavbarTwoColumns>
