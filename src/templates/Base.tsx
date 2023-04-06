4787e1bb5aee9686123195356d74c4681558cd4a
Hadi Rickit <rickithadi@gmail.com>
2023-04-06 10:51:57 +0700

    fix build errors

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,9 +1,9 @@
-import { Meta } from '../layout/Meta';
-import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
+import { Meta } from '../layout/Meta';
+import { AppConfig } from '../utils/AppConfig';
 
 const Base = () => (
   <div className="antialiased text-gray-600">
--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
+import { Logo } from './Logo';
 import { Background } from '../background/Background';
 import { CenteredFooter } from '../footer/CenteredFooter';
 import { Section } from '../layout/Section';
-import { Logo } from './Logo';
 
 const Footer = () => (
   <Background color="bg-gray-100">
