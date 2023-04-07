71fcc66cd50e06bd492811b42ef7f613577a5305
Hadi Rickit <rickithadi@gmail.com>
2023-04-07 14:00:24 +0700

    header columns

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -8,15 +8,21 @@ type IHeroOneButtonProps = {
 };
 
 const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center px-12">
-    <img src="/mainLanding.png" alt="logo" className="w-full h-half" />
-    <h1 className="pt-10 font-sans text-2xl text-gray-900 font-bold ">
-      {props.title}
-    </h1>
-    <div className="pt-3 font-lato text-base  mb-8 leading-normal 	">
-      {props.description}
+  <header className="text-center px-12 grid md:grid-cols-12 gap-8">
+    <img
+      src="/mainLanding.png"
+      alt="logo"
+      className="w-full order-first md:order-last mx-auto md:col-span-4 "
+    />
+    <div className="order-last md:order-first md:col-span-8 grid">
+      <h1 className="pt-10 font-sans text-2xl text-gray-900 font-bold md:text-left	md:text-[55px]">
+        {props.title}
+      </h1>
+      <div className="pt-3 font-lato text-base  mb-8 leading-normal md:text-left		">
+        {props.description}
+      </div>
+      <Button>Daftar Sekarang</Button>
     </div>
-    <Button>Daftar Sekarang</Button>
   </header>
 );
 
