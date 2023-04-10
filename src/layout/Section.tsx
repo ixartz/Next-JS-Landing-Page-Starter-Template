041164e035143e27bba4e1b2c2eb06857778e27f
Hadi Rickit <rickithadi@gmail.com>
2023-04-10 22:16:21 +0800

    header section laptop styles

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -13,26 +13,33 @@ const Section = (props: ISectionProps) => (
   <>
     <div
       className={`mx-auto
+      text-center
  ${props.grey ? `bg-heliotrope-70` : ''}`}
     >
       {(props.title || props.description) && (
         <div
-          className="px-12 text-center md:px-0 py-20 grid md:grid-cols-12 gap-8
+          className="px-12 text-center md:px-0 py-20 grid md:grid-cols-12 gap-8 mx-auto
+ max-w-screen-lg
       pb-14
       "
         >
-          <div className="md:col-span-4">
-            <h1 className="pb-6 leading-normal font-sans font-bold text-lg text-heliotrope-400 md:text-left">
+          <div className="md:col-span-5">
+            <h1 className="pb-3 leading-normal font-sans font-bold md:text-[24px] text-lg text-heliotrope-400 md:text-left">
               About us
             </h1>
             {props.title && (
-              <h2 className="text-2xl leading-normal font-sans text-gray-900 font-bold pb-4 md:text-left">
+              <h2 className=" text-[24px] md:text-[40px] leading-normal font-sans text-gray-900 font-bold  md:text-left">
                 {props.title}
               </h2>
             )}
           </div>
           {props.description && (
-            <div className="mt-4 text-base md:px-20 font-lato leading-[183%] md:col-span-8 md:text-left">
+            <div
+              className="
+           text-[16px]
+           md:text-2xl
+            font-lato leading-[160%] md:col-span-7 md:text-left"
+            >
               <span className="text-heliotrope-400">Playard</span>
               {props.description}
             </div>
