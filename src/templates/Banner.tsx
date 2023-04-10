baa01d0333a02a2994537cd818d3c10bed020e19
Hadi Rickit <rickithadi@gmail.com>
2023-04-10 22:57:57 +0800

    add color to footer, dummy contact form

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -7,8 +7,10 @@ import { Section } from '../layout/Section';
 const Banner = () => (
   <Section>
     <CTABanner
-      title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      subtitle="Start your Free Trial."
+      title="
+      Tingkatkan Gaya Hidup Aktifmu Bersama Kami! "
+      subtitle="
+      Daftar dalam waiting list dan dapatkan update saat kami sudah rilis"
       button={
         <Link href="https://creativedesignsguru.com/category/nextjs/">
           <a>
--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -6,7 +6,7 @@ import { CenteredFooter } from '../footer/CenteredFooter';
 import { Section } from '../layout/Section';
 
 const Footer = () => (
-  <Background color="bg-gray-100">
+  <Background color="bg-[#404040]">
     <Section>
       <CenteredFooter
         logo={<Logo />}
