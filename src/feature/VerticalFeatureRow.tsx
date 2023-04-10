9a5c0977e0b5436af55704ea3aaa2e034243368e
Hadi Rickit <rickithadi@gmail.com>
2023-04-10 22:49:46 +0800

    thinki got the wrong imagae order lol

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -1,8 +1,10 @@
+import { ReactNode } from 'react';
+
 import className from 'classnames';
 import { useRouter } from 'next/router';
 
 type IVerticalFeatureRowProps = {
-  title: string;
+  title: string | ReactNode;
   description: string;
   image: string;
   imageAlt: string;
@@ -11,10 +13,10 @@ type IVerticalFeatureRowProps = {
 
 const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
   const verticalFeatureClass = className(
-    'mt-20',
     'flex',
     'flex-wrap',
     'items-center',
+    'text-center px-6 md:px-12  grid md:grid-cols-12 gap-8',
     {
       'flex-row-reverse': props.reverse,
     }
@@ -24,14 +26,23 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 
   return (
     <div className={verticalFeatureClass}>
-      <div className="w-full sm:w-1/2 text-center sm:px-6">
+      <img
+        src={`${router.basePath}${props.image}`}
+        alt={props.imageAlt}
+        className="w-full mx-auto md:col-span-4 "
+      />
+      <div className="order-last md:order-first md:col-span-8 grid">
+        <h1 className=" font-sans text-2xl text-gray-900 font-bold md:text-left	md:text-[56px] leading-[148%]">
+          {props.title}
+        </h1>
+        <div className="pt-3 md:pt-4 font-lato text-base  mb-8 leading-normal md:text-left	md:text-[24px]	">
+          {props.description}
+        </div>
+      </div>
+      {/* <div className="w-full sm:w-1/2 text-center sm:px-6">
         <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
         <div className="mt-6 text-xl leading-9">{props.description}</div>
-      </div>
-
-      <div className="w-full sm:w-1/2 p-6">
-        <img src={`${router.basePath}${props.image}`} alt={props.imageAlt} />
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -10,22 +10,54 @@ const VerticalFeatures = () => (
     adalah platform untuk menghubungkan pencari dan penggemar olahraga untuk bermain bersama dengan mudah. Jadikan olahraga sebagai bagian dari gaya hidup kamu dan temukan teman baru dengan minat yang sama!"
   >
     <VerticalFeatureRow
-      title="Your title here"
+      title={
+        <>
+          {`Platform `}
+          <span className="text-heliotrope-400">Terbaik</span>
+          {` Untuk Mencari Lobi Olahraga\n`}
+        </>
+      }
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
+      image="/assets/images/landing1.png"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
+      title={
+        <>
+          {`Perbaharui Lifestyle Menjadi `}
+          <span className="text-heliotrope-400">Lebih Sehat</span>
+          {` Untuk Mencari Lobi Olahraga\n`}
+        </>
+      }
+      description="
+      Olahraga jadi lebih mudah, mau sendiri ataupun dengan teman sekarang bisa bermain olahraga kapanpun dan bersama orang lain."
+      image="/assets/images/landing2.png"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
+      title={
+        <>
+          {`Pilih Kawan dan Lawan `}
+          <span className="text-heliotrope-400">Sesuka Kamu</span>
+        </>
+      }
+      description="
+      Dengan klasifikasi level bermain yang beragam, kamu bisa bertemu kawan bermain yang pas atau tantang diri kamu di level yang lebih tinggi."
+      image="/assets/images/landing3.png"
+      imageAlt="Third feature alt text"
+    />
+    <VerticalFeatureRow
+      title={
+        <>
+          {`Buka dan Dapatkan UntungPlatform `}
+          <span className="text-heliotrope-400">Lobi</span>
+          {`dan Dapatkan `}
+          <span className="text-heliotrope-400">Untung</span>
+        </>
+      }
+      description="Dengan proses membuat lobi yang mudah, kamu bisa buat dan ramaikan lobi agar bisa dapat untung dari uang yang terkumpulkan."
+      image="/assets/images/landing4.png"
       imageAlt="Third feature alt text"
     />
   </Section>
