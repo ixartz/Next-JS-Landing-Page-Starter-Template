265bfe96199a66ff8be4365997b8258aadfc2860
Hadi Rickit <rickithadi@gmail.com>
2023-04-14 21:13:43 +0800

    done form

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -2,65 +2,76 @@ import React, { useState } from "react";
 
 import { useFormspark } from "@formspark/use-formspark";
 
-import { Background } from "./background/Background";
-
 const FORMSPARK_FORM_ID = "2K9HbgPn";
 
 export const ContactForm = () => {
   const [submit, submitting] = useFormspark({
     formId: FORMSPARK_FORM_ID,
   });
+  const [submitted, setSubmitted] = useState(false);
 
   const [message, setMessage] = useState("");
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
+    setSubmitted(true);
     await submit({ message });
-    alert("Form submitted");
   };
 
   return (
-    <Background color="bg-white">
-      <form
-        onSubmit={onSubmit}
-        className="p-10 text-center flex items-center mx-auto	"
-      >
-        <input
-          className="bg-[#F3F4F5] mx-auto text-center"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />{" "}
-        <button
-          type="submit"
-          disabled={submitting || !message}
-          className="mx-auto btn-primary btn btn-xl "
-        >
-          Send
-        </button>
-        <style jsx>
-          {`
-            .btn {
-              @apply inline-block  text-center rounded-full font-sans;
-            }
+    <div className="min-h-[10vh]">
+      {submitted ? (
+        <span className="text-heliotrope-400 py-5">Thank you!</span>
+      ) : (
+        <form onSubmit={onSubmit} className=" h-full">
+          <div className="relative ">
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              type="email"
+              id="email"
+              className="w-full block rounded-lg px-2.5 pb-2.5 pt-5 text-sm text-gray-900 bg-white border-2 border-[#DBDBDB] appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+            <label
+              htmlFor="email"
+              className="p-2 absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Email
+            </label>
+          </div>
 
-            .btn-base {
-              @apply text-[16px] md:text-[20px] font-semibold;
-            }
+          <button
+            type="submit"
+            disabled={submitting || !message}
+            className="btn-primary btn btn-base md:float-right my-5"
+          >
+            <p className="px-10 py-2 md:px-12">Daftar </p>
+          </button>
+          <style jsx>
+            {`
+              .btn {
+                @apply inline-block  text-center rounded-full font-sans;
+              }
 
-            .btn-xl {
-              @apply font-extrabold text-xl py-4 px-6;
-            }
+              .btn-base {
+                @apply text-[16px]  font-semibold;
+              }
 
-            .btn-primary {
-              @apply text-white bg-heliotrope-400;
-            }
+              .btn-xl {
+                @apply font-extrabold text-xl py-4 px-6;
+              }
 
-            .btn-primary:hover {
-              @apply bg-heliotrope-500;
-            }
-          `}
-        </style>
-      </form>
-    </Background>
+              .btn-primary {
+                @apply text-white bg-heliotrope-400;
+              }
+
+              .btn-primary:hover {
+                @apply bg-heliotrope-500;
+              }
+            `}
+          </style>
+        </form>
+      )}
+    </div>
   );
 };
--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -7,25 +7,27 @@ type IHeroOneButtonProps = {
   description: string;
 };
 
-const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center px-12 md:px-0  grid md:grid-cols-12 gap-8 pb-14 md:pb-20">
-    <img
-      src="/mainLanding.png"
-      alt="logo"
-      className="w-full order-first md:order-last mx-auto md:col-span-4 "
-    />
-    <div className="order-last md:order-first md:col-span-8 grid">
-      <h1 className="pt-10 font-sans text-2xl text-gray-900 font-bold md:text-left	md:text-[56px] leading-[148%]">
-        {props.title}
-      </h1>
-      <div className="pt-3 md:pt-4 font-lato text-base  mb-8 leading-normal md:text-left	md:text-[24px]	">
-        {props.description}
+const HeroOneButton = (props: IHeroOneButtonProps) => {
+  return (
+    <header className="text-center px-12 md:px-0  grid md:grid-cols-12 gap-8 pb-14 md:pb-20">
+      <img
+        src="/mainLanding.png"
+        alt="logo"
+        className="w-full order-first md:order-last mx-auto md:col-span-4 "
+      />
+      <div className="order-last md:order-first md:col-span-8 grid">
+        <h1 className="pt-10 font-sans text-2xl text-gray-900 font-bold md:text-left	md:text-[56px] leading-[148%]">
+          {props.title}
+        </h1>
+        <div className="pt-3 md:pt-4 font-lato text-base  mb-8 leading-normal md:text-left	md:text-[24px]	">
+          {props.description}
+        </div>
+        <div className="text-center md:text-left">
+          <Button> Daftar Sekarang</Button>
+        </div>
       </div>
-      <div className="text-center md:text-left">
-        <Button>Daftar Sekarang</Button>
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export { HeroOneButton };
