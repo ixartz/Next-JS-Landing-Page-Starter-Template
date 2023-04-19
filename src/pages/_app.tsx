a1fdf552db8c9da8f2d295ed80a2da89c9b0103f
Hadi Rickit <rickithadi@gmail.com>
2023-04-19 09:26:51 +0800

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,20 @@ import { AppProps } from "next/app";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-QD8ZTM7F1V"
+    ></script>
+    <script>
+      {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QD8ZTM7F1V');`}
+    </script>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
