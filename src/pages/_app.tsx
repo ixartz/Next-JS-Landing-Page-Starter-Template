446e3efc9186a856a26527aa7e17f9c4a49b28bb
Hadi Rickit <rickithadi@gmail.com>
2023-04-19 11:09:32 +0800

    move gtag into document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,6 @@ import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-QD8ZTM7F1V"
-    ></script>
-    <script>
-      {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-QD8ZTM7F1V');`}
-    </script>
     <Component {...pageProps} />
   </>
 );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,17 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QD8ZTM7F1V"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QD8ZTM7F1V');`}
+        </script>
         <body>
           <Main />
           <NextScript />
