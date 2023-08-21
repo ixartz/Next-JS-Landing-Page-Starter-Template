a5c4d376ddebfcfc5d9f00997029a1657260dc11
muffafa <mm.savar@gmail.com>
2023-08-22 00:22:10 +0300

    [update]: link updated

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = () => (
       title="Aklındaki bir çok soru için,"
       subtitle="bizimle iletişime geç!"
       button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
+        <Link href="https://linktr.ee/yazilimcukurova">
           <Button>İletişim İçin Tıkla</Button>
         </Link>
       }
