2836e12b6bb6936ff1a716f98c2044e504ec59d0
muffafa <mm.savar@gmail.com>
2023-08-22 00:03:48 +0300

    [update]: sitename, title , dscrp & local updated

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'Yazılım Çukurova',
+  title: 'Yazılım Çukurova Topluluğu (YÇ)',
+  description: 'Yazılıma ilgi duyuyorsan doğru yerdesin!',
+  locale: 'tr',
 };
