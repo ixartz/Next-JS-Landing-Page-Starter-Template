54ba31e6cff2eb15a3e7d6a43d35c6b6eeb2b70e
Aaron Fay <aaron.j.fay@gmail.com>
2023-08-26 10:26:52 -0700

    fix TH language code

--- src/templates/LibraryContentLanguage.tsx
+++ src/templates/LibraryContentLanguage.tsx
@@ -5,7 +5,7 @@ type ILibraryContentLanguageProps = {
 const LibraryContentLanguage = (props: ILibraryContentLanguageProps) => (
   <>
     {props.language.includes('crk') && <span className="p-1 font-bold">Y</span>}
-    {props.language.includes('cwk') && (
+    {props.language.includes('cwd') && (
       <span className="p-1 font-bold">TH</span>
     )}
     {props.language.includes('eng') && <span className="p-1 font-bold">E</span>}
