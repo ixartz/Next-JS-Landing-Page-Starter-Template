6cdcfd9fdd47c73eef553a62f4c882f82197acfe
Aaron Fay <aaron.j.fay@gmail.com>
2023-08-25 19:47:40 -0700

    update icons, add back podcast

--- src/templates/LibraryIcon.tsx
+++ src/templates/LibraryIcon.tsx
@@ -19,7 +19,7 @@ const LibraryIcon = (props: ILibraryIconProps) => (
         />
       </svg>
     )}
-    {props.type === 'audio' && (
+    {props.type === 'podcast' && (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -96,6 +96,22 @@ const LibraryIcon = (props: ILibraryIconProps) => (
           strokeLinecap="round"
           strokeLinejoin="round"
           d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+        />
+      </svg>
+    )}
+    {props.type === 'audio' && (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
         />
       </svg>
     )}
