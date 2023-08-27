a586a2592bffcb084f30479dd73560bca9fbfda5
Aaron Fay <aaron.j.fay@gmail.com>
2023-08-27 10:58:32 -0700

    add description to library

--- src/templates/Library.tsx
+++ src/templates/Library.tsx
@@ -10,7 +10,12 @@ const Library = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <TopNav></TopNav>
-    <Section title="Learner library">{}</Section>
+    <Section title="Learner library">
+      <p className="text-center">
+        A (non-exhaustive) collection of resources for language learners,
+        largely focused around the Cree language.
+      </p>
+    </Section>
     <LibraryDataTable></LibraryDataTable>
     <Footer />
   </div>
