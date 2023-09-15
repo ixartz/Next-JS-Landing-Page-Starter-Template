208a8701a41301824af531cea1420bdbc98353eb
Aaron Fay <aaron.j.fay@gmail.com>
2023-09-15 09:16:18 -0700

    move analytics to the _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import '../styles/global.css';
 
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <NextUIProvider>
+    <GoogleAnalytics trackPageViews gaMeasurementId="G-L21H4M24PB" />
     <Component {...pageProps} />
   </NextUIProvider>
 );
