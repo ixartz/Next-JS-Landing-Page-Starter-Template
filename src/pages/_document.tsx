bbc97254a53af7eb09232e0c6542f37106e750e5
Aaron Fay <aaron.j.fay@gmail.com>
2023-09-15 09:08:28 -0700

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,11 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 import { Providers } from '@/app/providers';
 
 import { AppConfig } from '../utils/AppConfig';
+
+const GA_MEASUREMENT_ID = 'G-L21H4M24PB';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
@@ -11,6 +14,18 @@ class MyDocument extends Document {
     return (
       <Html lang={AppConfig.locale}>
         <Head />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <body>
           <Providers>
             <Main />
