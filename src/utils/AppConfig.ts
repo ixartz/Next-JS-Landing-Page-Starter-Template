57744abfaafa3e8d45edf0d9923bd750b0546e4f
LuckyHariharan <lharihar@uwaterloo.ca>
2023-09-19 11:06:14 -0400

    update husky

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'LuckyHariharan',
+  title: 'Portfolio',
+  description: 'Luckys Portfolio of Software Development',
   locale: 'en',
 };
