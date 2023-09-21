59fd8635ddd9a5e57ab9b2a547a871b01a015800
LuckyHariharan <lharihar@uwaterloo.ca>
2023-09-21 11:42:44 -0400

    Update CenteredFooter.tsx

--- src/footer/CenteredFooter.tsx
+++ src/footer/CenteredFooter.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react';
 
-import { FooterCopyright } from './FooterCopyright';
 import { FooterIconList } from './FooterIconList';
 
 type ICenteredFooterProps = {
@@ -21,10 +20,6 @@ const CenteredFooter = (props: ICenteredFooterProps) => (
 
     <div className="mt-8 flex justify-center">
       <FooterIconList>{props.iconList}</FooterIconList>
-    </div>
-
-    <div className="mt-8 text-sm">
-      <FooterCopyright />
     </div>
 
     <style jsx>
