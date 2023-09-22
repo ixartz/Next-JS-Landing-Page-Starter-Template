ddaeb665fdbedc566033978f7eb52486ac140b7e
LuckyHariharan <lharihar@uwaterloo.ca>
2023-09-22 16:31:59 -0400

    Redirect button to projectwork

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -2,30 +2,32 @@ import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
-  <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
-  >
-    <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
-      imageAlt="First feature alt text"
-    />
-    <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
-      imageAlt="Second feature alt text"
-      reverse
-    />
-    <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
-    />
-  </Section>
+  <div id="ProjectWork">
+    <Section
+      title="Portfolio of Projects"
+      description="See my Android, Frontend and Insurtech Projects here"
+    >
+      <VerticalFeatureRow
+        title="Insurtech Project"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+        image="/assets/images/feature.svg"
+        imageAlt="First feature alt text"
+      />
+      <VerticalFeatureRow
+        title="React Native App - CRUD APP"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+        image="/assets/images/feature2.svg"
+        imageAlt="Second feature alt text"
+        reverse
+      />
+      <VerticalFeatureRow
+        title="Skillify Personality Quiz"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+        image="/assets/images/feature3.svg"
+        imageAlt="Third feature alt text"
+      />
+    </Section>
+  </div>
 );
 
 export { VerticalFeatures };
