04e090dff01f20fd8bb82664ccdf762007e9f9bf
Marcus Souza <souzamrcsv@gmail.com>
2023-11-25 14:44:37 -0300

    deleting logo

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -3,12 +3,11 @@ import Link from 'next/link';
 import { Background } from '../background/Background';
 import { CenteredFooter } from '../footer/CenteredFooter';
 import { Section } from '../layout/Section';
-import { Logo } from './Logo';
 
 const Footer = () => (
   <Background color="bg-gray-100">
     <Section>
-      <CenteredFooter logo={<Logo />} iconList={undefined}>
+      <CenteredFooter logo={undefined} iconList={undefined}>
         <li>
           <Link href="/">Home</Link>
         </li>
