bfcb28d7c9dc652c5be26adbeecaaac7e6329459
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-16 12:12:13 -0400

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -14,12 +14,9 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
+          {/* <Link href="/">
+            <Button>Request for Demo</Button>
+          </Link> */}
         </li>
       </NavbarTwoColumns>
     </Section>
