775a9183f8c41e2f0eccce7b2b4ccc26556d10c0
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-16 15:20:58 -0400

    Update FooterCopyright.tsx

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -2,14 +2,7 @@ import { AppConfig } from '../utils/AppConfig';
 
 const FooterCopyright = () => (
   <div className="footer-copyright">
-    © Copyright {new Date().getFullYear()} {AppConfig.site_name}.{' '}
-    <a href="https://creativedesignsguru.com">Website Template</a>.
-    {/*
-     * PLEASE READ THIS SECTION
-     * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-     * The link doesn't need to appear on every pages, one link on one page is enough.
-     * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-     */}
+    © Copyright {new Date().getFullYear()} {AppConfig.site_name}.
     <style jsx>
       {`
         .footer-copyright :global(a) {
