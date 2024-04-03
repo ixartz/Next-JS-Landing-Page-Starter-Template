425322cedda5b343bfb00c6a15b423771f892599
Aaron Fay <aaron.j.fay@gmail.com>
2024-04-02 23:11:37 -0700

    adjust the copyright

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -2,8 +2,7 @@ import { AppConfig } from '../utils/AppConfig';
 
 const FooterCopyright = () => (
   <div className="footer-copyright">
-    © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-    <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
+    © Copyright {new Date().getFullYear()} {AppConfig.title}.
     {/*
      * PLEASE READ THIS SECTION
      * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
