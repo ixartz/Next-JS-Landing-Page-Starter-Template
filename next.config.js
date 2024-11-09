e6aacb29a8bb935d66c6017995379f0611b6b8ee
Aaron Fay <aaron.j.fay@gmail.com>
2024-11-09 11:20:55 -0800

    adjust build for next 14

--- next.config.js
+++ next.config.js
@@ -14,7 +14,9 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     loader: 'akamai',
     path: '',
   },
