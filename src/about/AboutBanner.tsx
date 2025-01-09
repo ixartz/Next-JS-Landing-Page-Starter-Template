7162be6127891c7d451f8fc077feece33edfab13
Aaron Fay <aaron.j.fay@gmail.com>
2025-01-09 10:09:30 -0800

    add tiktok link

--- src/about/AboutBanner.tsx
+++ src/about/AboutBanner.tsx
@@ -25,27 +25,47 @@ const AboutBanner = (props: IAboutBannerProps) => (
     </div>
 
     <div className="p-4 text-xl font-semibold">
-      <div className="btn btn-base btn-primary cursor-pointer">
-        <a
-          href="https://m.me/115148727058478"
-          target="_blank"
-          className="rounded-md bg-primary-500 p-3 px-5 text-white hover:bg-primary-400"
-        >
-          Contact me
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-7 pl-3"
-            style={{ display: 'inline-block' }}
+      <div className="flex justify-center gap-4">
+        <div className="btn btn-base btn-primary cursor-pointer">
+          <a
+            href="https://m.me/115148727058478"
+            target="_blank"
+            className="rounded-md bg-primary-500 p-3 px-5 text-white hover:bg-primary-400"
           >
-            <path
-              fillRule="evenodd"
-              d="M3.43 2.524A41.29 41.29 0 0110 2c2.236 0 4.43.18 6.57.524 1.437.231 2.43 1.49 2.43 2.902v5.148c0 1.413-.993 2.67-2.43 2.902a41.202 41.202 0 01-5.183.501.78.78 0 00-.528.224l-3.579 3.58A.75.75 0 016 17.25v-3.443a41.033 41.033 0 01-2.57-.33C1.993 13.244 1 11.986 1 10.573V5.426c0-1.413.993-2.67 2.43-2.902z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
+            Contact me
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-7 pl-3"
+              style={{ display: 'inline-block' }}
+            >
+              <path
+                fillRule="evenodd"
+                d="M3.43 2.524A41.29 41.29 0 0110 2c2.236 0 4.43.18 6.57.524 1.437.231 2.43 1.49 2.43 2.902v5.148c0 1.413-.993 2.67-2.43 2.902a41.202 41.202 0 01-5.183.501.78.78 0 00-.528.224l-3.579 3.58A.75.75 0 016 17.25v-3.443a41.033 41.033 0 01-2.57-.33C1.993 13.244 1 11.986 1 10.573V5.426c0-1.413.993-2.67 2.43-2.902z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+        <div className="btn btn-base btn-primary cursor-pointer">
+          <a
+            href="https://www.tiktok.com/@otipeyimisow"
+            target="_blank"
+            className="rounded-md bg-black p-3 px-5 text-white hover:bg-gray-800"
+          >
+            Find me on TikTok
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              fill="currentColor"
+              className="h-5 w-7 pl-3"
+              style={{ display: 'inline-block' }}
+            >
+              <path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3v178.8a162.6 162.6 0 1 1 -141.6-161v89.3a74.6 74.6 0 1 0 52.2 71.2V0h88.2a121.2 121.2 0 0 0 1.9 22.2 122.2 122.2 0 0 0 53.9 80.2 121.4 121.4 0 0 0 68.2 20.8z" />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   </div>
--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -11,7 +11,7 @@ Aaron nitisiyihkâson, asinîwâciy-wâskahikan ohci niya. 👋
 const subDescription = `
 My name is Aaron, I'm software engineer and language learner of nêhiyawêwin (Plains Cree, Y-dialect). 
 I grew up in the Rocky Mountain House area in Treaty 6 territory, my family is Métis of Red River descent (Caplette/Lafournaise) and môniyâw.
-I've been learning nêhiyawêwin for about 5 years now, and as a self-study I know how hard it is to acquire an Indigenous second language as an adult. 
+I've been learning nêhiyawêwin for about 6 years now, and as a self-study I know how hard it is to acquire an Indigenous second language as an adult. 
 For that reason I've tried to put together some resources here in an attempt to aide other language learners in their journey. 
 `;
 
