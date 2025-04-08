0ba4ca3e4ea6a64e8bd9d384ee1315dcc61aa624
Rémi Weng <ixartz@users.noreply.github.com>
2025-04-08 20:15:56 +0200

    chore: update sponsor links and images in component

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -12,6 +12,32 @@ const Sponsors = () => (
         <tr className="h-56">
           <td className="border-2 border-gray-300 p-3">
             <a
+              href="https://clerk.com?utm_source=github&utm_medium=sponsorship&utm_campaign=nextjs-boilerplate"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/clerk-logo-dark.png"
+                alt="Clerk – Authentication & User Management for Next.js"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+          <td className="border-2 border-gray-300 p-3">
+            <a
+              href="https://www.coderabbit.ai?utm_source=next_js_starter&utm_medium=github&utm_campaign=next_js_starter_oss_2025"
+              target="_blank"
+            >
+              <Image
+                src="/assets/images/coderabbit-logo-light.svg"
+                alt="CodeRabbit"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+          <td className="border-2 border-gray-300 p-3">
+            <a
               href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
               target="_blank"
             >
@@ -23,52 +49,13 @@ const Sponsors = () => (
               />
             </a>
           </td>
-          <td className="border-2 border-gray-300 p-3">
-            <a
-              href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate"
-              target="_blank"
-            >
-              <Image
-                src="/assets/images/better-stack-dark.png"
-                alt="Better Stack"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
-          <td className="border-2 border-gray-300 p-3">
-            <a href="https://nextlessjs.com">
-              <Image
-                src="/assets/images/nextlessjs.png"
-                alt="React SaaS Boilerplate Next.js"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
         </tr>
         <tr className="h-56">
           <td className="border-2 border-gray-300 p-3">
-            <a
-              href="https://clerk.com?utm_source=github&utm_medium=sponsorship&utm_campaign=nextjs-boilerplate"
-              target="_blank"
-            >
+            <a href="https://launch.arcjet.com/Q6eLbRE" target="_blank">
               <Image
-                src="/assets/images/clerk.png"
-                alt="Clerk – Authentication & User Management for Next.js"
-                width={260}
-                height={224}
-              />
-            </a>
-          </td>
-          <td className="border-2 border-gray-300 p-3">
-            <a
-              href="https://turso.tech/?utm_source=nextjsstarterbp"
-              target="_blank"
-            >
-              <Image
-                src="/assets/images/turso.png"
-                alt="SQLite Developer Experience"
+                src="/assets/images/arcjet-light.svg"
+                alt="Arcjet"
                 width={260}
                 height={224}
               />
@@ -77,8 +64,18 @@ const Sponsors = () => (
           <td className="border-2 border-gray-300 p-3">
             <a href="https://l.crowdin.com/next-js" target="_blank">
               <Image
-                src="https://support.crowdin.com/assets/logos/core-logo/svg/crowdin-core-logo-cDark.svg"
-                alt="Crowdin"
+                src="/assets/images/crowdin-dark.png"
+                alt="Better Stack"
+                width={260}
+                height={224}
+              />
+            </a>
+          </td>
+          <td className="border-2 border-gray-300 p-3">
+            <a href="https://nextjs-boilerplate.com/pro-saas-starter-kit">
+              <Image
+                src="/assets/images/nextjs-boilerplate-saas.png"
+                alt="Next.js Boilerplate SaaS"
                 width={260}
                 height={224}
               />
