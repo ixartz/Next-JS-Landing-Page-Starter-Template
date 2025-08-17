24ca8cfe10cd7a3e39ce31e59a5192805aa1f735
Aaron Fay <aaron.j.fay@gmail.com>
2025-08-17 16:47:45 -0700

    fix centering

--- src/templates/SupportUs.tsx
+++ src/templates/SupportUs.tsx
@@ -5,22 +5,17 @@ import { Coffee } from './Coffee';
 const SupportUs = () => (
   <div className="rounded-md bg-green-100 text-center">
     <Section>
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between sm:pb-12 sm:text-left">
-        <div>
-          <h2
-            id="support"
-            className="pb-4 text-center text-3xl font-bold text-emerald-800"
-          >
-            Support Us!
-          </h2>
-          <div className="text-center text-base text-emerald-700">
-            <strong>All our projects are free to use</strong>! . Every dollar
-            donated goes towards the development of language projects. <br />
-            We appreciate your support 🙏
-          </div>
+      <div className="text-center">
+        <h2 id="support" className="pb-4 text-3xl font-bold text-emerald-800">
+          Support Us!
+        </h2>
+        <div className="text-base text-emerald-700">
+          <strong>All our projects are free to use</strong>! . Every dollar
+          donated goes towards the development of language projects. <br />
+          We appreciate your support 🙏
         </div>
       </div>
-      <div className="pb-4 text-center">
+      <div className="pb-4 pt-8 text-center">
         <Coffee></Coffee>
       </div>
     </Section>
